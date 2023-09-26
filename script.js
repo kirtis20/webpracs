68c68171f89fe9f90f1a5798b0e2dd85ec641161
kirtis20 <146112395+kirtis20@users.noreply.github.com>
2023-09-26 21:18:02 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,73 +1,106 @@
-$(document).ready(function () {
-    console.log("Document ready"); // Check if this is logged
-
-    $("#btnCalc").click(function () {
-        console.log("Calculate button clicked"); // Check if this is logged
-
-        // Calculate the unit cost based on coffee type
-        var unitCost = 0.0;
-
-        {if ($("#optRegular").prop("checked")) {
-            unitCost = 15.5;
-        } else if ($("#optFilter").prop("checked")) {
-            unitCost = 20.5;
-        } else if ($("#optCappuccino").prop("checked")) {
-            unitCost = 25;
-        } else if ($("#optLatte").prop("checked")) {
-            unitCost = 30;
-        } 
+function formValidation()
+{
+    
+    var uid = document.getElementById('userid');
+    var passid = document.getElementById('passid');
+    var uname = document.getElementById('username');
+    var uadd = document.getElementById('address');
+    var ucountry = document.getElementById('country');
+    if(userid_validation(uid,5,12))
+    {   
+        if(passid_validation(passid,7,12))                  
+        {
+            if(allAlpha(uname))
+            {
+                if(alphanumeric(uadd))
+                { 
+                    if(countryselect(ucountry))
+                    {
+                         alert("validated");
+                    }
+                }
+            }
+             
+        }
     }
-        // ... Add more coffee types here ...
-
-        console.log("Unit cost: " + unitCost); // Check the value of unitCost
-
-        // Get the user's input
-        var amount = parseFloat($('#txtQuant').val());
-        if (isNaN(amount)) {
-            amount = 0;
-        }
-        var selectedSize = $("#coffeeSize").val();
-        var selectedFlavor = $("#coffeeFlavor").val();
-
-        console.log("Amount: " + amount); // Check the value of amount
-        console.log("Selected size: " + selectedSize); // Check the value of selectedSize
-        console.log("Selected flavor: " + selectedFlavor); // Check the value of selectedFlavor
-
-        // Calculate size and flavor multipliers
-        var sizeMultiplier = 1.0;
-        var flavorMultiplier = 1.0;
-
-        if (selectedSize === "medium") {
-            sizeMultiplier = 1.2;
-        } else if (selectedSize === "large") {
-            sizeMultiplier = 1.5;
-        }
-
-        if (selectedFlavor === "vanilla") {
-            flavorMultiplier = 1.2;
-        } else if (selectedFlavor === "chocolate") {
-            flavorMultiplier = 1.3;
-        } else if (selectedFlavor === "caramel") {
-            flavorMultiplier = 1.4;
-        }
-
-        console.log("Size multiplier: " + sizeMultiplier); // Check the value of sizeMultiplier
-        console.log("Flavor multiplier: " + flavorMultiplier); // Check the value of flavorMultiplier
-
-        // Calculate the total price
-        var pprice=unitCost * sizeMultiplier * flavorMultiplier;
-        var total = amount * unitCost * sizeMultiplier * flavorMultiplier;
-        total = total.toFixed(2);
-
-        console.log("Total: " + total); // Check the value of total
-        $('#txtPrice').val(pprice);
-        // Update the total field
-        $('#txtTotal').val(total);
-    });
-
-    // Code for displaying the cookie
-    $("#freebie").click(function () {
-        console.log("Freebie clicked"); // Check if this is logged
-        $("#cookie").slideToggle("slow");
-    });
-});
+    return false;
+}
+//-----------------------------------------------------------
+//user id validator function
+function userid_validation(uid,m_min,m_max)
+{
+    var uid_len = uid.value.length;
+    // alert("Inside user_id validator");
+    if (uid_len == 0 || uid_len >= m_max || uid_len < m_min)    
+    {
+        alert("User Id should not be empty / length be between "+m_min+" to "+m_max);
+        document.getElementById('errorname').innerHTML="this is an invalid uid";
+        uid.focus();
+        return false;
+    }
+    return true;
+}
+//------------------------------------------------------
+//password id validator function
+function passid_validation(passid,mx,my)
+{
+    alert("Inside pass_id validator");
+    var passid_len = passid.value.length;
+    if (passid_len == 0 ||passid_len >= my || passid_len < mx)
+    {
+        alert("Password should not be empty / length be between "+mx+" to "+my);
+        document.getElementById('errorpass').innerHTML="this is an invalid password";
+        passid.focus();
+        return false;
+    }
+    return true;
+}
+//-----------------------------------------------------------
+function allAlpha(uname)
+{ 
+    var letters = /^[A-Za-z\s]+$/;
+    if(uname.value.match(letters))
+    {
+        return true;
+    }
+    else
+    {
+        alert('Username must have alphabet characters only');
+        document.getElementById('erroruname').innerHTML="this is an invalid user name";
+        uname.focus();
+        return false;
+    }
+}
+//----------------------------------------------
+//user address
+function alphanumeric(uadd)
+{ 
+    var letters = /^[a-zA-Z0-9\s,]+$/;
+    
+    if(uadd.value.match(letters))
+    {
+        return true;
+    }
+    else
+    {
+        alert('User address must have alphanumeric characters only');
+        document.getElementById('erroraddress').innerHTML="this is an invalid address";
+        uadd.focus();
+        return false;
+    }
+}
+//------------------------------------------------------------------
+function countryselect(ucountry)
+{
+    if(ucountry.value == "Default")
+    {
+     alert('Select your country from the list');
+     document.getElementById('errorcountry').innerHTML="this is an invalid address";     
+     ucountry.focus();
+     return false;
+    }
+    else
+    {
+    return true;
+    }
+}
